02d1a1a4789573ceb7ebda06f41054de72f70c23
Vishwasa Navada K <vishwasa.navada@sagerx.com>
2023-04-26 17:43:50 +0530

    refactor: bette error handling

--- index.js
+++ index.js
@@ -3,25 +3,29 @@ import Parser from 'node-sql-parser';
 
 try {
   const parser = new Parser.Parser();
-  const data = fs.readFileSync('./input/load_departments.dump', 'utf8');
+  const data = fs.readFileSync('./input/load_employees.dump', 'utf8');
   const ast = parser.astify(data);
   ast.forEach((astElement) => {
-    switch (astElement.type) {
-      case 'insert':
-        console.log('\nInsert Queries from the file');
-        astElement.values.forEach((insertValue) => {
-          console.log(
-            `${astElement.table[0].table}.create({${insertValue.value
+    if (astElement.columns.length) {
+      switch (astElement.type) {
+        case 'insert':
+          console.log('\nInsert Queries from the file');
+          let insertFQL = ``;
+          astElement.values.forEach((insertValue) => {
+            insertFQL += `${astElement.table[0].table}.create({${insertValue.value
               .map((column, idx) => `${astElement.columns[idx]}: '${column.value}'`)
-              .join(',')}})`
-          );
-        });
-        break;
-      case 'select':
-        console.log('\nSelect Queries from the file');
-        break;
-      default:
-        console.log('Not a correct type');
+              .join(',')}})\n`;
+          });
+          console.log(insertFQL);
+          break;
+        case 'select':
+          console.log('\nSelect Queries from the file');
+          break;
+        default:
+          console.log('Not a correct type');
+      }
+    } else {
+      console.log('Column Names are missing insert');
     }
   });
 } catch (err) {

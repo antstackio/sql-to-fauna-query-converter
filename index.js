0dab0cfff1b52026f697f4595cc9ca0cb4ccd94d
Vishwasa Navada K <vishwasa.navada@sagerx.com>
2023-05-23 09:57:57 +0530

    chore: remove commented example

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import Parser from 'node-sql-parser';
 
 try {
   const parser = new Parser.Parser();
-  const data = fs.readFileSync('./input/load_employees.dump', 'utf8');
+  const data = fs.readFileSync('./input/load_salaries1.dump', 'utf8');
   const ast = parser.astify(data);
   ast.forEach((astElement) => {
     if (astElement.columns.length) {
       switch (astElement.type) {
         case 'insert':
-          console.log('\nInsert Queries from the file');
+          // console.log('\nInsert Queries from the file');
           let insertFQL = ``;
           astElement.values.forEach((insertValue) => {
             insertFQL += `${astElement.table[0].table}.create({${insertValue.value
@@ -31,52 +31,3 @@ try {
 } catch (err) {
   console.error(err);
 }
-
-/*!SECTION
-{
-    "type": "insert",
-    "table": [
-        {
-            "db": null,
-            "table": "departments",
-            "as": null
-        }
-    ],
-    "columns": [
-        "dept_no",
-        "dept_name"
-    ],
-    "values": [
-        {
-            "type": "expr_list",
-            "value": [
-                {
-                    "type": "single_quote_string",
-                    "value": "d001"
-                },
-                {
-                    "type": "single_quote_string",
-                    "value": "Marketing"
-                }
-            ]
-        },
-        {
-            "type": "expr_list",
-            "value": [
-                {
-                    "type": "single_quote_string",
-                    "value": "d002"
-                },
-                {
-                    "type": "single_quote_string",
-                    "value": "Finance"
-                }
-            ]
-        }
-    ],
-    "partition": null,
-    "prefix": "into",
-    "on_duplicate_update": null
-}
-
-*/

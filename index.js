dbc4b2ea224ada35acc99702d521adcbaf292691
Vishwasa Navada K <vishwasa.navada@sagerx.com>
2023-05-23 10:20:04 +0530

    feat: change logic to read the input directory recursively and process the files

--- index.js
+++ index.js
@@ -1,32 +1,46 @@
 import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 import Parser from 'node-sql-parser';
 
 try {
   const parser = new Parser.Parser();
-  const data = fs.readFileSync('./input/load_salaries1.dump', 'utf8');
-  const ast = parser.astify(data);
-  ast.forEach((astElement) => {
-    if (astElement.columns.length) {
-      switch (astElement.type) {
-        case 'insert':
-          // console.log('\nInsert Queries from the file');
-          let insertFQL = ``;
-          astElement.values.forEach((insertValue) => {
-            insertFQL += `${astElement.table[0].table}.create({${insertValue.value
-              .map((column, idx) => `${astElement.columns[idx]}: '${column.value}'`)
-              .join(',')}})\n`;
-          });
-          console.log(insertFQL);
-          break;
-        case 'select':
-          console.log('\nSelect Queries from the file');
-          break;
-        default:
-          console.log('Not a correct type');
+  const inputDir = path.resolve(`${__dirname}/input`);
+  const outputDir = path.resolve(path.join(__dirname, './output'));
+
+  const sqlFileList = fs.readdirSync(inputDir, (err, files) =>
+    files.filter((e) => path.extname(e).toLowerCase() === '.sql')
+  );
+  sqlFileList.forEach((sqlFile) => {
+    const data = fs.readFileSync(`${inputDir}/${sqlFile}`, 'utf8');
+    const ast = parser.astify(data);
+    ast.forEach((astElement) => {
+      if (astElement.columns.length) {
+        let FQLOutput = ``;
+        switch (astElement.type) {
+          case 'insert':
+            astElement.values.forEach((insertValue) => {
+              FQLOutput += `${astElement.table[0].table}.create({${insertValue.value
+                .map((column, idx) => `${astElement.columns[idx]}: '${column.value}'`)
+                .join(',')}})\n`;
+            });
+            // console.log(insertFQL);
+            break;
+          case 'select':
+            console.log('\nSelect Queries from the file');
+            break;
+          default:
+            console.log('Not a correct type');
+        }
+        // Write the output to file in Output Directory
+        fs.writeFileSync(`${outputDir}/${sqlFile.replace('.sql', '.fql')}`, FQLOutput);
+      } else {
+        console.log('Column Names are missing insert');
       }
-    } else {
-      console.log('Column Names are missing insert');
-    }
+    });
   });
 } catch (err) {
   console.error(err);
